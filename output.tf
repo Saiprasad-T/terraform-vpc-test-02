0e7f7376ea703862a3fd044181e306b76e223a8c
SaiThota <thotasaiprasad180@gmail.com>
2026-03-19 15:27:46 +0530

    module

--- output.tf
+++ output.tf
@@ -2,4 +2,15 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
+output "natgtw_id" {
+  value       = aws_nat_gateway.main.id
+}
+
+output "igw_id" {
+  value       = aws_internet_gateway.igw.id
+}
+
+output "eip_pub_ip" {
+  value       = aws_eip.nat_eip.public_ip
+}
 
